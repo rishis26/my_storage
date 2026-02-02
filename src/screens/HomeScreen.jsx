abe0385f3ed9472b417b134ca6cfacf836ca0fb0
rishis26 <rishishah781@gmail.com>
2026-02-02 23:45:21 +0530

    added HomeScreen code in the folder

---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    padding: '4%',
+    backgroundColor: '#ffffff',
+  },
+});
