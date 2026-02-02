2936d15455a93187b8476e8b9d3c766ac57428f9
rishis26 <rishishah781@gmail.com>
2026-02-02 23:44:35 +0530

    added home screen in app.jsx file

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import HomeScreen from './src/screens/HomeScreen';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>App</Text>
+    <View>
+      <HomeScreen />
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-})
+const styles = StyleSheet.create({});
