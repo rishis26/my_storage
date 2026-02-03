6da98ce79a219af3da3891330b507d418de350bf
rishis26 <rishishah781@gmail.com>
2026-02-03 10:58:25 +0530

    added 3 buttons in the dashboard

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,9 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
+      <Text style={styles.title}>Dashboard</Text>
+      <View style={styles.buttonContainer}>
+        <Pressable style={styles.button}>
+          <Text style={styles.btnText}>All Items</Text>
+        </Pressable>
+        <Pressable style={styles.button}>
+          <Text style={styles.btnText}>Low stock</Text>
+        </Pressable>
+        <Pressable style={styles.button}>
+          <Text style={styles.btnText}>Create</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
@@ -16,5 +27,26 @@ const styles = StyleSheet.create({
     height: '100%',
     padding: '8%',
     backgroundColor: '#ffffff',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  buttonContainer: {
+    marginVertical: 10,
+    flexDirection: 'row',
+    gap: 10,
+  },
+  button: {
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 50,
+    borderWidth: 0.8,
+    borderColor: 'green',
+  },
+  btnText: {
+    color: 'green',
+    fontSize: 12,
   },
 });
